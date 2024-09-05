f9157dc48c854ede0e050face1df0d37f4a7114f
alejandrogamezg <alejandrogamezg@gmail.com>
2024-09-05 17:50:51 -0500

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'API REST FULL!  -  Utilizar REST para conectar sistemas basados en HTTP para obtener y generar datos, y devolverlos en formato JSON. ';
   }
 }
